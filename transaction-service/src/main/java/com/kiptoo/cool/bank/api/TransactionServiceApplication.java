9323c96ed74df09a8ef73e382bec38390545a76d
Kiptoo  Moses <kiptoomoses156@gmail.com>
2025-06-30 11:22:13 +0300

    corrected transaction service

--- transaction-service/src/main/java/com/kiptoo/cool/bank/api/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/kiptoo/cool/bank/api/TransactionServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/kiptoo/cool/bank/api/repository/UserRepository.java
+++ user-service/src/main/java/com/kiptoo/cool/bank/api/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByEmailAndPassword(String email, String password);
+   
 
     User save(User user);
 }
